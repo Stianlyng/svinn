10b91686660f147533383599f5e0ad5182f497fc
Stian Lyng <stianlyng@protonmail.com>
2023-03-21 00:49:08 +0100

    Init dev DB

---
+++ src/main/java/ntnu/idatt2105/stianlyng/svinn/SvinnApplication.java
@@ -0,0 +1,15 @@
+package ntnu.idatt2105.stianlyng.svinn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SvinnApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SvinnApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/ntnu/idatt2105/stianlyng/svinn/SvinnApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2105.stianlyng.svinn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvinnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
