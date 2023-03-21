8678a6f740d7548075e6198dd3a48671875802cf
Stianlyng <stianlyng@protonmail.com>
2023-03-21 19:39:42 +0100

    Fix Auto Increment

--- src/main/java/ntnu/idatt2105/stianlyng/svinn/model/User.java
+++ src/main/java/ntnu/idatt2105/stianlyng/svinn/model/User.java
@@ -10,7 +10,8 @@ import jakarta.persistence.Table;
 @Table(name = "users")
 public class User {
   @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
+  //@GeneratedValue(strategy=GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
 
   private String name;
