7a569a639ff421ee1bca6feff385996ade92d967
Stian Lyng <stianlyng@protonmail.com>
2023-03-25 06:21:44 +0100

    Add pinia

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import { useAuthStore } from '../stores/authStore';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,37 +8,53 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
       path: '/sell',
       name: 'sell',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/SellView.vue')
+      component: () => import('../views/SellView.vue'),
     },
     {
       path: '/bookmarks',
       name: 'bookmarks',
-      component: () => import('../views/BookmarksView.vue')
+      component: () => import('../views/BookmarksView.vue'),
     },
     {
       path: '/messages',
       name: 'messages',
-      component: () => import('../views/MessageView.vue')
+      component: () => import('../views/MessageView.vue'),
     },
     {
       path: '/profile',
       name: 'profile',
-      component: () => import('../views/ProfileView.vue')
+      component: () => import('../views/ProfileView.vue'),
     },
     {
       path: '/my-listings',
       name: 'my-listings',
-      component: () => import('../views/MyListingsView.vue')
+      component: () => import('../views/MyListingsView.vue'),
     },
-  ]
-})
+  ],
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+  const isAuthenticated = !!authStore.token;
+  const protectedRoutes = [
+    'sell',
+    'bookmarks',
+    'messages',
+    'profile',
+    'my-listings',
+  ];
+
+  if (protectedRoutes.includes(to.name) && !isAuthenticated) {
+    // Redirect to the login or register page if the user is not authenticated
+    next({ name: 'home' });
+  } else {
+    next();
+  }
+});
+
+export default router;
